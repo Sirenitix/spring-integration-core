d78693fd42d545d2f4b90557fc5d5040324154b4
nx <suleevn@gmail.com>
2022-07-04 11:30:42 +0600

    vXsTlYqIkUjMv

--- src/test/java/com/nurs/core/SpringIntegrationTests.java
+++ src/test/java/com/nurs/core/SpringIntegrationTests.java
@@ -3,7 +3,7 @@ package com.nurs.core;
 import com.nurs.core.dao.OrderRepository;
 import com.nurs.core.dao.PaymentRepository;
 import com.nurs.core.dto.OrderRequest;
-import com.nurs.core.dto.PaymentRequest;
+import com.nurs.core.dto.TestPaymentRequest;
 import com.nurs.core.dto.UpdateOrderRequest;
 import com.nurs.core.entity.Order;
 import com.nurs.core.entity.Payment;
@@ -25,11 +25,12 @@ import reactor.core.publisher.Mono;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SpringIntegrationTests {
 
 	@Autowired
@@ -54,50 +55,51 @@ class SpringIntegrationTests {
 		mockWebServer.start();
 	}
 
-
-	@Test
-	@org.junit.jupiter.api.Order(1)
-	void createOrder() {
-		BigDecimal amount = new BigDecimal("1000.00");
-		String email = "n.suleev@yandex.ru";
-		OrderRequest orderRequest = new OrderRequest(amount,email);
-		webClient.post().uri("localhost:9090/order")
-				.accept(MediaType.APPLICATION_JSON)
-				.body(Mono.just(orderRequest), OrderRequest.class)
-				.exchange()
-				.expectStatus().isOk();
-		Order actualOrder = orderRepository.findByEmail(email);
-		assertThat(email).isEqualTo(actualOrder.getEmail());
-		assertThat(amount).isEqualTo(actualOrder.getAmount());
+	@AfterEach
+	void deleteEntities() {
+		paymentRepository.deleteAll();
 		orderRepository.deleteAll();
 	}
 
 
+	@Test
+	void deleteOrder() throws InterruptedException {
+		createOrder();
+		int id = 1;
+		webClient.delete().uri("localhost:9090/order/" + id )
+				.exchange()
+				.expectStatus().isOk();
+		TimeUnit.SECONDS.sleep(1);
+		Long orderId = 1L;
+		Order order = orderRepository.findById(orderId).orElse(null);
+		assertThat(order).isEqualTo(null);
+	}
+
 
 	@Test
-	@org.junit.jupiter.api.Order(2)
-	void updateOrder() {
+	void updateOrder() throws InterruptedException {
 		createOrder();
-
 		Boolean testPaid = true;
 		String testMail = "test@mail.com";
 		String testDate = LocalDate.now().toString();
 		BigDecimal testAmount = new BigDecimal("500.00");
 
 		UpdateOrderRequest updateOrder = new UpdateOrderRequest();
+		updateOrder.setId(1L);
 		updateOrder.setDate(testDate);
 		updateOrder.setAmount(testAmount);
 		updateOrder.setPaid(testPaid);
 		updateOrder.setEmail(testMail);
 
-		int id = 1;
+		int id = 2;
 		webClient.put().uri("localhost:9090/order/" + id )
 				.accept(MediaType.APPLICATION_JSON)
 				.body(Mono.just(updateOrder), UpdateOrderRequest.class)
 				.exchange()
 				.expectStatus().isOk();
+		TimeUnit.SECONDS.sleep(1);
 
-		Long orderId = 1L;
+		Long orderId = 2L;
 		Order order = orderRepository.findById(orderId).orElse(null);
 
 		log.info(order + " - expected order");
@@ -106,48 +108,50 @@ class SpringIntegrationTests {
 		assertThat(order.getAmount()).isEqualTo(testAmount);
 		assertThat(order.getDate()).isEqualTo(testDate);
 		assertThat(order.getEmail()).isEqualTo(testMail);
-		orderRepository.deleteAll();
 	}
 
+
 	@Test
-	@org.junit.jupiter.api.Order(3)
-	void payOrder() {
-		createOrder();
-		Long testOrderId = 1L;
-		String testCard = "4636790463393611";
-
-
-		PaymentRequest paymentRequest = new PaymentRequest();
-		paymentRequest.setOrderId(testOrderId);
-		paymentRequest.setCreditCardNumber(testCard);
-
-		int id = 1;
-		webClient.post().uri("localhost:9090/order/" + id + "/payment")
+	void createOrder() throws InterruptedException {
+		BigDecimal amount = new BigDecimal("1000.00");
+		String email = "n.suleev@yandex.ru";
+		OrderRequest orderRequest = new OrderRequest(amount,email);
+		webClient.post().uri("localhost:9090/order")
 				.accept(MediaType.APPLICATION_JSON)
-				.body(Mono.just(paymentRequest), PaymentRequest.class)
+				.body(Mono.just(orderRequest), OrderRequest.class)
 				.exchange()
 				.expectStatus().isOk();
+		TimeUnit.SECONDS.sleep(1);
+		Order actualOrder = orderRepository.findByEmail(email);
+		assertThat(email).isEqualTo(actualOrder.getEmail());
+		assertThat(amount).isEqualTo(actualOrder.getAmount());
+	}
 
+
+	@Test
+	void payOrder() throws InterruptedException {
+		createOrder();
+		Long testOrderId = 4L;
+		String testCard = "4146431922133966";
+
+
+		TestPaymentRequest testPaymentRequest = new TestPaymentRequest();
+		testPaymentRequest.setCreditCardNumber(testCard);
+
+
+		webClient.post().uri("localhost:9090/order/"+ testOrderId.intValue() +"/payment")
+				.accept(MediaType.APPLICATION_JSON)
+				.body(Mono.just(testPaymentRequest), TestPaymentRequest.class)
+				.exchange()
+				.expectStatus().isOk();
+		TimeUnit.SECONDS.sleep(1);
 		Order order = orderRepository.findById(testOrderId).orElse(null);
 		Payment payment = paymentRepository.findByOrder(order);
 		assertThat(payment.getOrder().getId()).isEqualTo(testOrderId);
 		assertThat(payment.getCreditCardNumber()).isEqualTo(testCard);
-		paymentRepository.deleteAll();
-		orderRepository.deleteAll();
 	}
 
-	@Test
-	@org.junit.jupiter.api.Order(4)
-	void deleteOrder() {
-		createOrder();
-		int id = 1;
-		webClient.delete().uri("localhost:9090/order/" + id )
-				.exchange()
-				.expectStatus().isOk();
-		Long orderId = 1L;
-		Order order = orderRepository.findById(orderId).orElse(null);
-		assertThat(order).isEqualTo(null);
-	}
+
 
 
 
